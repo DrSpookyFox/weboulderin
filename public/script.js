3d57af58b38da1b0c0f4cc00df3410a2d4fe6c3f
Alexander Singer <96636056+DrSpookyFox@users.noreply.github.com>
2023-07-09 17:14:25 -0400

    recieved current from weather API

---
+++ public/script.js
@@ -0,0 +1,17 @@
+window.onload = (e) => {
+  console.log("page is fully loaded");
+  fetch("/weather", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    // body: json.stringify({
+    // this is where you can write longitutude, latitude, and other things
+    // }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const WEATHER_API= process.env.WEATHER_API
-const express = require('express')
-const app = express()
+const WEATHER_API = process.env.WEATHER_API;
+const { default: axios } = require("axios");
+const express = require("express");
+const app = express();
 
-app.use(express.json())
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.static("public"));
 
-app.post('/weather', (req,res) =>{
-
-})
+app.post("/weather", (req, res) => {
+console.log(req.body)
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=41.747532&lon=-74.086891&appid=${WEATHER_API}
+    `;
+  axios({
+    url: url,
+    responseType: "json",
+  }).then((data) => res.json(data.data.current));
+});
 
 app.listen(3000, () => {
-    console.log('Server Started')
-  })
-
+  console.log("Server Started");
+});
